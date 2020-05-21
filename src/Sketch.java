c795247ed872c14ff35455f7ee6ece48c91d3c16
Adrianna Gilmore <a_inman33@yahoo.com>
2020-05-21 00:09:10 -0700

    wondering if this commit is working

---
+++ src/Sketch.java
@@ -0,0 +1,17 @@
+import processing.core.PApplet;
+
+public class Sketch extends PApplet{
+
+    public void settings() {
+        size(500,500);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+    }
+
+
+}
